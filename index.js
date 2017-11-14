a5b20bf60459f5096625a5759a0b3b98d50170e1
Tim Shipman <bricklayertim@yahoo.com>
2017-11-14 21:22:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ function lowerCaseDrivers(array){
     return driver.toLowerCase()
   })
 }
+
+function nameToAttributes(names){
+  names.map(function(name){
+    let fName = name.split(" ")[0]
+    let lName = name.split(" ")[1]
+    return {firstName: fName, lastName: lName}
+  })
+}
